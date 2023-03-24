37aa09b178caab59774f246a96b2829060139b4f
Excel951 <abigailexcelsisdeo@gmail.com>
2023-03-24 13:47:35 +0700

    penyelesaian cashier

--- app/src/main/java/com/example/simplecashier/MainActivity.java
+++ app/src/main/java/com/example/simplecashier/MainActivity.java
@@ -1,42 +1,73 @@
 package com.example.simplecashier;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView hasilView;
+    private EditText hasilView;
     private Button getEsTeh, getEsJeruk, getNasiPecel, getNasiRawon, getBayar, getBatal;
-    private int hasil=0, esTehvalue=3000, esJerukvalue=4000, nasiPecelvalue=10000, nasiRawonvalue=12000;
+    private int hasil = 0, esTehvalue = 3000, esJerukvalue = 4000, nasiPecelvalue = 10000, nasiRawonvalue = 12000;
+
+    private View mainActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getBayar=(Button) findViewById(R.id.byrBtn);
-        getBatal=(Button) findViewById(R.id.btlBtn);
-        getEsTeh=(Button) findViewById(R.id.esTehBtn);
-        getEsJeruk=(Button) findViewById(R.id.esJerukBtn);
-        getNasiPecel=(Button) findViewById(R.id.nasiPecelBtn);
-        getNasiRawon=(Button) findViewById(R.id.nasiRawonBtn);
-        hasilView=(TextView) findViewById(R.id.hasil);
+        getBayar = (Button) findViewById(R.id.byrBtn);
+        getBatal = (Button) findViewById(R.id.btlBtn);
+        getEsTeh = (Button) findViewById(R.id.esTehBtn);
+        getEsJeruk = (Button) findViewById(R.id.esJerukBtn);
+        getNasiPecel = (Button) findViewById(R.id.nasiPecelBtn);
+        getNasiRawon = (Button) findViewById(R.id.nasiRawonBtn);
+        hasilView = (EditText) findViewById(R.id.hasil);
+        hasilView.setEnabled(false);
+        mainActivity = (View) findViewById(R.id.myCoordinatorLayout);
+
+
+        getBayar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text;
+                if (hasil == 0) {
+                    text = "Masih Belum Ada Tagihan";
+                } else {
+                    text = "Pembayaran Sebesar Rp " + hasil + " Berhasil Dibayar";
+                    hasil = 0;
+                }
+                hasilView.setText(String.valueOf(hasil));
+                Snackbar byrSukses = Snackbar.make(mainActivity, text, Snackbar.LENGTH_LONG);
+                byrSukses.show();
+            }
+        });
 
         getBatal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                hasil=0;
+                String text;
+                if (hasil == 0) {
+                    text = "Masih Belum Ada Tagihan";
+                } else {
+                    text = "Tagihan Dibatalkan";
+                    hasil = 0;
+                }
                 hasilView.setText(String.valueOf(hasil));
+                Snackbar tagihanBtl = Snackbar.make(mainActivity, text, Snackbar.LENGTH_LONG);
+                tagihanBtl.show();
             }
         });
 
         getEsTeh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                hasil=hasil+esTehvalue;
+                hasil = hasil + esTehvalue;
                 hasilView.setText(String.valueOf(hasil));
             }
         });
@@ -44,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
         getEsJeruk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                hasil=hasil+esJerukvalue;
+                hasil = hasil + esJerukvalue;
                 hasilView.setText(String.valueOf(hasil));
             }
         });
@@ -52,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
         getNasiPecel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                hasil=hasil+nasiPecelvalue;
+                hasil = hasil + nasiPecelvalue;
                 hasilView.setText(String.valueOf(hasil));
             }
         });
@@ -60,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
         getNasiRawon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                hasil=hasil+nasiRawonvalue;
+                hasil = hasil + nasiRawonvalue;
                 hasilView.setText(String.valueOf(hasil));
             }
         });
